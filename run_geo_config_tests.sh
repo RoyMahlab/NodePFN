13934011850f471e98ddd74404d941ecdc3a6cf1
roy mahlab <roy.mahlab@gmail.com>
2026-08-10 19:42:36 +0300

    fix: updated training run command

--- run_geo_config_tests.sh
+++ run_geo_config_tests.sh
@@ -3,22 +3,30 @@
 CONFIG_PATH=configs
 NUM_STEPS=2048
 
-uv run python pretrain_and_node_classification.py \
-    --model_name full_baseline \
-    --prior causal \
-    --gpus 1 \
-    --epochs 50 \
-    --num_steps $NUM_STEPS \
-    --batch_size 1 \
-    --max_num_classes 100
+# uv run python pretrain_and_node_classification.py \
+#     --model_name full_baseline \
+#     --prior causal \
+#     --gpus 1 \
+#     --epochs 50 \
+#     --num_steps $NUM_STEPS \
+#     --batch_size 1 \
+#     --max_num_classes 100
 
-for config_file in "$CONFIG_PATH/geo_baseline"*.json; do
+configs=(
+  configs/geo_baseline.json
+  configs/geo_baseline_less_features.json
+  # configs/geo_baseline_less_geo_features.json
+  # configs/geo_baseline_low_gamma_dist.json
+  # configs/geo_baseline_mid_gamma_dist.json
+)
+
+for config_file in "${configs[@]}"; do
   echo "$config_file"
   uv run python pretrain_and_node_classification.py \
     --model_name $config_file \
     --prior geo \
     --gpus 1 \
-    --epochs 50 \
+    --epochs 30 \
     --num_steps $NUM_STEPS \
     --batch_size 1 \
     --max_num_classes 100 \
